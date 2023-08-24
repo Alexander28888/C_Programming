f13e8daffef9cf0792bce250acc6639716949ebd
Alexander28888 <39930877+Alexander28888@users.noreply.github.com>
2023-08-24 16:31:57 -0600

    Add files via upload

---
+++ printing.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int two =2 , three =3, six = 6;
+    int result = (two+three)*six/three;
+    printf("I'm new doing this, and the result is:%d",result);
+    
+}
